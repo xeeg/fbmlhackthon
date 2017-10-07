12f84b28d7ce6be1b1e95a51c621bec21c37512c
Sophie Dankel <sophiedankel@gmail.com>
2017-10-07 13:03:19 -0700

    Implement get_zipcode()

--- add_features.py
+++ add_features.py
@@ -1,7 +1,8 @@
+#!/usr/bin/python
+
 # -*- coding: utf-8 -*-
 
 import config
-import model_stats
 
 import os, sys, yaml, logging
 import time
@@ -9,22 +10,27 @@ import time
 import numpy as np
 import pandas as pd
 
+from geopy.geocoders import Nominatim
+
+def get_zipcode(addr_string):
+    geolocator = Nominatim()
+    location = geolocator.geocode(addr_string, addressdetails=True)
+    return location.raw['address']['postcode'];
+
 def get_zipcode(long, lat):
-    return 98052;
+    geolocator = Nominatim()
+    location = geolocator.reverse(lat, long)
+    return location.raw['address']['postcode'];
 
 def add_features(df, year, zipcode):
-
-    df['zipcode'] = get_zipcode(df['long'], df['lat'])
-    
+    df['zipcode'] = get_zipcode(df['lat'], df['long'])
     return df
 
-   
-def main():
-
+def __main__():
     start_time = time.time()
 
     """Reads configuration and adds features to the raw data."""
-    
+
     options = config.get_config()
     logging.debug('Reading input %s' % options.input)
     df = pd.read_pickle(options.input)
@@ -32,9 +38,8 @@ def main():
 
     df = add_features(df)
     df.to_pickle(options.processed)
-    
+
     print("--- Execution time:  %s seconds ---" % (time.time() - start_time))
-    
+
 if __name__ == "__main__":
     main()
-
